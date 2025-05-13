42b20c72db08e23af1471b34f74e92e679c2011d
whereareiam <mmm.g032@gmail.com>
2025-05-13 19:17:34 +0200

    feat: update Template to latest Socialismus API

--- src/main/java/me/whereareiam/socialismus/module/template/Module.java
+++ src/main/java/me/whereareiam/socialismus/module/template/Module.java
@@ -2,40 +2,38 @@ package me.whereareiam.socialismus.module.template;
 
 import com.google.inject.Inject;
 import com.google.inject.name.Named;
-import me.whereareiam.socialismus.api.output.LoggingHelper;
+import me.whereareiam.socialismus.api.Logger;
 import me.whereareiam.socialismus.api.output.module.SocialisticModule;
 
 import java.nio.file.Path;
 
 public class Module extends SocialisticModule {
-    private final LoggingHelper loggingHelper;
-    private final Path dataPath;
+	private final Path dataPath;
 
-    @Inject
-    public Module(LoggingHelper loggingHelper, @Named("dataPath") Path dataPath) {
-        this.loggingHelper = loggingHelper;
-        this.dataPath = dataPath;
-    }
+	@Inject
+	public Module(@Named("dataPath") Path dataPath) {
+		this.dataPath = dataPath;
+	}
 
-    @Override
-    public void onLoad() {
-        loggingHelper.info("Module loaded!");
-        loggingHelper.info("Data path: " + dataPath);
-        loggingHelper.info("Module path: " + workingDirectory);
-    }
+	@Override
+	public void onLoad() {
+		Logger.info("Module loaded!");
+		Logger.info("Data path: " + dataPath);
+		Logger.info("Module path: " + workingPath);
+	}
 
-    @Override
-    public void onEnable() {
+	@Override
+	public void onEnable() {
 
-    }
+	}
 
-    @Override
-    public void onDisable() {
+	@Override
+	public void onDisable() {
 
-    }
+	}
 
-    @Override
-    public void onUnload() {
+	@Override
+	public void onUnload() {
 
-    }
+	}
 }
